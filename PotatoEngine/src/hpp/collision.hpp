d20f0120a0d4f5aa7e9338e9f945dfd66120d0b4
julio delgado <jjdelgadoabreu@student.fullsail.edu>
2022-02-28 23:17:41 -0500

    add aabb_t struct and bound method

--- PotatoEngine/src/hpp/collision.hpp
+++ PotatoEngine/src/hpp/collision.hpp
@@ -9,6 +9,41 @@ namespace dxe {
 		float radius{ 0.f };
 	};
 
+	struct aabb_t {
+		union {
+			glm::vec3 center{ 0.f };
+			glm::vec3 min;
+		};
+		bool isMinMax{ false };
+		union {
+			glm::vec3 extent{ 0.f };
+			glm::vec3 max;
+		};
+	};
+
+	inline bool ComputeBounds(const aabb_t& ls, const aabb_t& rs, aabb_t& result) {
+		// invalid input, boxes must be in the same format
+		if (ls.isMinMax != rs.isMinMax) { return false; }
+
+		result.isMinMax = ls.isMinMax;
+
+		if (ls.isMinMax) { 
+			// for the min max format we just need the smallest and largest values
+			result.min = glm::min(ls.min, rs.min);
+			result.max = glm::max(ls.max, ls.max);
+
+		} else {
+
+			result.max = glm::max(glm::vec3(ls.center + ls.extent), glm::vec3(rs.center + rs.extent));
+			result.min = glm::min(ls.center, rs.center);
+
+			result.center = glm::vec3(result.min + result.max) / 2.f;
+			result.extent = glm::vec3(result.max - result.center);
+		}
+
+		return true;
+	}
+
 	inline const bool RayToSphereCollision(const glm::vec3 pos, glm::vec3 direction, const sphere target, float& time) {
 
 		glm::vec3 targetTopos = pos - target.pos;
--- PotatoEngine/src/hpp/game_scene.hpp
+++ PotatoEngine/src/hpp/game_scene.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "object_data.hpp"
 #include "input.hpp"
 #include "collision.hpp"
 #include "scene.hpp"
@@ -37,34 +36,13 @@ namespace dxe {
 
 		void update(const float dt);
 
-		/*const GameObject* GetSceneObjects() const;
-		const GameObject* GetSkyBox() const;
-		const uint32_t GetObjectTotal() const;
-		const View_t& GetView() const;
-		const Scene_cb& GetSceneBuffer() const;
-		const Textwrap* GetTextUI() const;
-		const uint32_t GetTextUITotal() const;
-		Emitter* GetParticles();*/
-
 	private:
 
 		void inputUpdate(const float dt);
 
-		// View_t camera;
-
-		// std::vector<GameObject> gameObjects;
-
-		// GameObject plane;
-
-		// GameObject skyBox;
-
-		// Scene_cb scb;
-
 		static constexpr uint8_t MAX_ENEMIES = 10;
 
 		enemy enemies[MAX_ENEMIES];
-
-		// std::vector<Textwrap> textui;
 
 		uint32_t score{ 0 };
 		
